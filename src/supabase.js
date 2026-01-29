3f2e91f2ba56e3cc077353967fbf4696616c7113
ffrancojoao <ffrancojoao@gmail.com>
2026-01-29 14:27:50 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://abusgasorwsaoahmjvyl.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFidXNnYXNvcndzYW9haG1qdnlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MDI4MjYsImV4cCI6MjA4NTI3ODgyNn0.lmDfWXnKlNw4HMTnlD3dnMyCuZy3tjJNANyVTpE-MKw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
